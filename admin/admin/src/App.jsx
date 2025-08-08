abf1a1797ce41277692666b44ad63de4b67f3c98
Shivam kumar <shivam68338@gmail.com>
2025-08-08 11:51:40 +0530

    Update App.jsx

--- admin/admin/src/App.jsx
+++ admin/admin/src/App.jsx
@@ -8,7 +8,7 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-  const url = " http://localhost:4000";
+  const url = "https://delivery-app-ojjm.onrender.com";
   return (
     <div>
       <ToastContainer />
