4d40c2ac35e8be2740c6b0f126918da1ce295ebc
Shivam kumar <shivam68338@gmail.com>
2025-08-14 22:47:58 +0530

    Update FoodDisplay.jsx

--- Frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ Frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,7 +1,7 @@
-import "./foodDisplay.css";
+import "./FoodDisplay.css";
 import { useContext } from "react";
-import { StoreContext } from "../../context/StoreContext";
-import FoodItem from "../FoodItem/FoodItem";
+import { StoreContext } from "../../context/StoreContext.js";
+import FoodItem from "../FoodItem/FoodItem.js";
 
 const FoodDisplay = ({ category }) => {
   const { food_list } = useContext(StoreContext);
