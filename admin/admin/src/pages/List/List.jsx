a94e313d499d902df4770a69927e9801efc999bb
Shivam kumar <shivam68338@gmail.com>
2025-08-14 22:28:10 +0530

    Update List.jsx

--- admin/admin/src/pages/List/List.jsx
+++ admin/admin/src/pages/List/List.jsx
@@ -49,7 +49,7 @@ const List = ({ url }) => {
         {list.map((item, index) => {
           return (
             <div key={index} className="list-table-format">
-              <img src={`${url}/images/` + item.image} alt={item.name} />
+              <img src={item.imageUrl} alt={item.name} />
               <p>{item.name}</p>
               <p>{item.category}</p>
               <p>{item.price}</p>
