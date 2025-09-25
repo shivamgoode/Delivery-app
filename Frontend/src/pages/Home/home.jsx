adc97c37ecb3433080c2f34ccb2ee35b311d3a88
Shivam kumar <shivam68338@gmail.com>
2025-09-25 22:36:38 +0530

    Update home.jsx

--- Frontend/src/pages/Home/home.jsx
+++ Frontend/src/pages/Home/home.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import "./home.css";
-import Header from "../../components/Header/Header.jsx";
-import ExploreMenu from "../../components/ExploreMenu/ExploreMenu.jsx";
-import FoodDisplay from "../../components/FoodDisplay/FoodDisplay.jsx";
-import AppDownload from "../../components/AppDownload/AppDownload.jsx";
+import Header from "../../components/Header/Header";
+import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
+import FoodDisplay from "../../components/FoodDisplay/FoodDisplay";
+import AppDownload from "../../components/AppDownload/AppDownload";
 
 const Home = () => {
   const [category, setCategory] = useState("All");
