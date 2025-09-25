af36e1e71d74af335e8a64f7a76dedb4f0e5e003
Shivam kumar <shivam68338@gmail.com>
2025-09-25 22:44:07 +0530

    Update orderRoute.js

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import auth from '../middleware/auth.js';
-import {placeOrder,verifyOrder, userOrders,listOrders,updateStatus} from '../controllers/orderController.js';
+import {placeOrder,verifyOrder, userOrders,listOrders,updateStatus, codOrder} from '../controllers/orderController.js';
 
 
 const orderRouter = express.Router();
@@ -10,6 +10,7 @@ orderRouter.post('/verify', verifyOrder);
 orderRouter.post('/userorders', auth, userOrders);
 orderRouter.get('/list', listOrders);
 orderRouter.post('/status',  updateStatus);
+orderRouter.post('/cod',auth, codOrder)
 
 
 
